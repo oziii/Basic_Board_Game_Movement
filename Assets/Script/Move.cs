a0cc1ba49251a677bf97cd918f14f795208658f3
oğuzhan karademir <genc__gezgin@hotmail.com>
2020-04-10 19:04:13 +0300

    first commit

---
+++ Assets/Script/Move.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Move : MonoBehaviour
+{
+    public int steps;
+    public int coin;
+
+    public GameObject currentNode;
+    public GameObject wheelObject;
+    public Text coinText;
+    public List<GameObject> nodeList;
+
+    public IEnumerator MoveTo()
+    {
+        steps = wheelObject.GetComponent<Spin>().stepNumber;
+
+        for (int i = 0; i < steps; i++)
+        {
+            if(nodeList.Count != (nodeList.IndexOf(currentNode))+1) 
+            { 
+                transform.position = new Vector3(
+                    nodeList[(nodeList.IndexOf(currentNode))+1].transform.position.x,
+                    transform.position.y,
+                    nodeList[(nodeList.IndexOf(currentNode))+1].transform.position.z);
+                currentNode = nodeList[(nodeList.IndexOf(currentNode)) + 1];
+            }
+            else if(nodeList.Count == (nodeList.IndexOf(currentNode)) + 1)
+            { 
+                transform.position = new Vector3(nodeList[0].transform.position.x,
+                    transform.position.y, 
+                    nodeList[0].transform.position.z);
+                currentNode = nodeList[0];
+            }
+            coin += 10;
+            coinText.text = coin.ToString(); 
+            yield return new WaitForSeconds(0.7f);
+        }
+
+    }
+}
---
+++ Assets/Script/Spin.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Spin : MonoBehaviour
+{
+	int randVal;
+	private float timeInterval;
+	private bool isCoroutine;
+	public int finalAngle;
+	public int stepNumber;
+
+	public int section;
+	float totalAngle;
+
+	private void Start()
+	{
+		isCoroutine = true;
+		totalAngle = 360 / section;
+	}
+	private void Update()
+	{
+
+		if (Input.GetMouseButton(0) && isCoroutine)
+		{
+			StartCoroutine(SpinWheel());		
+		}
+	}
+
+	private IEnumerator SpinWheel()
+	{
+
+		isCoroutine = false;
+		randVal = Random.Range(200, 300);
+
+		timeInterval = 0.0001f * Time.deltaTime * 2;
+
+		for (int i = 0; i < randVal; i++)
+		{
+
+			transform.Rotate(0, 0, (totalAngle / 2));
+
+			if (i > Mathf.RoundToInt(randVal * 0.2f))
+				timeInterval = 0.5f * Time.deltaTime;
+			if (i > Mathf.RoundToInt(randVal * 0.5f))
+				timeInterval = 1f * Time.deltaTime;
+			if (i > Mathf.RoundToInt(randVal * 0.7f))
+				timeInterval = 1.5f * Time.deltaTime;
+			if (i > Mathf.RoundToInt(randVal * 0.8f))
+				timeInterval = 2f * Time.deltaTime;
+			if (i > Mathf.RoundToInt(randVal * 0.9f))
+				timeInterval = 2.5f * Time.deltaTime;
+
+			yield return new WaitForSeconds(timeInterval);
+
+		}
+
+		if (Mathf.RoundToInt(transform.eulerAngles.z) % totalAngle != 0) 
+			transform.Rotate(0, 0, totalAngle / 2);
+
+		finalAngle = Mathf.RoundToInt(transform.eulerAngles.z);
+		stepNumber = (finalAngle / (section*10) )+1;
+		print(stepNumber);
+
+		
+
+		isCoroutine = true;
+		StartCoroutine(GameObject.Find("Hero").GetComponent<Move>().MoveTo());
+	}
+
+}
